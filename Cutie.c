00ae969686f99cc94f870fd0a58817e291b7a8ab
K Vivek Kumar <102508246+K-Vivek-Kumar@users.noreply.github.com>
2023-03-28 01:12:31 +0530

    Update Cutie.c

--- Cutie.c
+++ Cutie.c
@@ -9,70 +9,70 @@ Run it thrice for better results.
 
 #include <stdio.h>
 
-int main() {
-    int i,j,n;
+int main()
+{
+    int i, j, n;
     char name[20];
-    printf ("Enter your beautiful name:");
-    scanf ("%s",&name);
-    for (int i=0;i<8;i++)
-    {printf ("_");}
-    printf ("Hi!");
-for (int i=0;i<8;i++)
-    {printf ("_");}
-    printf ("\n");
-    printf ("  ");
-    printf ("\(\\___/)");
-    printf ("    ");
-    printf ("\(\\___/)");
-    printf ("\n");
-    printf ("  ");
-    printf ("\( *.");
-printf ("*");
-printf(" \)");
-     printf ("    ");
-     printf ("\( *.");
-printf("*");
-printf(" \)");
-     printf ("\n");
-     printf ("  ");
-     printf ("\ />");
-     
-     printf (" How are you ");
-     printf ("\<");
+    printf("Enter your beautiful name:");
+    scanf("%s", &name);
+    for (int i = 0; i < 8; i++)
+    {
+        printf("_");
+    }
+    printf("Hi!");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("_");
+    }
+    printf("\n");
+    printf("  ");
+    printf("\(\\___/)");
+    printf("    ");
+    printf("\(\\___/)");
+    printf("\n");
+    printf("  ");
+    printf("\( *.");
+    printf("*");
+    printf(" \)");
+    printf("    ");
+    printf("\( *.");
+    printf("*");
+    printf(" \)");
+    printf("\n");
+    printf("  ");
+    printf("\ />");
 
+    printf(" How are you ");
+    printf("\<");
 
-     printf ("\\");
-     
-      printf("\n");
-      printf ("      ");
-printf("❤%s ji❤",name);
+    printf("\\");
 
+    printf("\n");
+    printf("      ");
+    printf("❤%s ji❤", name);
 
-printf("\n");
-     printf ("__");
-     printf ("\( ");
-     printf ("  ");
-     printf ("\>");
-     printf ("\>");
-     printf (" ");
-     printf ("\)");
-     
-     
-     
-    printf (" ");
-     
-     printf ("__");
-     printf (" ");
-     printf ("\(");
-     printf (" ");
-     printf ("\<");
-     printf ("\<");
-     printf ("  ");
-     printf ("\)");
-      
-     printf ("_");
-     printf ("\n");
-     printf ("_______Bye!_________");
-    
-return 0;
+    printf("\n");
+    printf("__");
+    printf("\( ");
+    printf("  ");
+    printf("\>");
+    printf("\>");
+    printf(" ");
+    printf("\)");
+
+    printf(" ");
+
+    printf("__");
+    printf(" ");
+    printf("\(");
+    printf(" ");
+    printf("\<");
+    printf("\<");
+    printf("  ");
+    printf("\)");
+
+    printf("_");
+    printf("\n");
+    printf("_______Bye!_________");
+    return 0;
 }
